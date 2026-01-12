3c2926c65b890f417c7bbe78dbca664fa0ab221d
boddepallihemavarun <153508117+boddepallihemavarun@users.noreply.github.com>
2026-01-12 13:34:28 +0530

    Time: 1 ms (98.92%), Space: 46.5 MB (39.23%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Solution {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+
+        // base case
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        // try each number as root
+        for (int i = start; i <= end; i++) {
+
+            List<TreeNode> leftTrees = build(start, i - 1);
+            List<TreeNode> rightTrees = build(i + 1, end);
+
+            // combine left & right
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+}
